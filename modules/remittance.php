a0b54d71945ae543b118affae1776bd647c218c6
Sly Flores <sly@christian.com.ph>
2016-08-31 21:53:24 +0800

    Remove +8 in date

--- modules/remittance.php
+++ modules/remittance.php
@@ -5,8 +5,8 @@ require '../grants.php';
 $store_branch = $_SESSION['branch'];
 
 $co = "First Cutoff";
-if ((date("D",strtotime("+8 Hours")) == "Sat") || (date("D",strtotime("+8 Hours")) == "Sun")) $co = "End of the day";
-//if (date("D",strtotime("+8 Hours")) == "Fri") $co = "End of the day";
+if ((date("D") == "Sat") || (date("D") == "Sun")) $co = "End of the day";
+//if (date("D") == "Fri") $co = "End of the day";
 
 ?>
 <div id="sub-menu">
@@ -28,7 +28,7 @@ if ((substr_count($USER_GRANTS,$ADMIN_GRANT) == 1) || (substr_count($USER_GRANTS
 <option value="1" <?php if ($store_branch == 1) echo "selected=\"selected\"";?> >Francey</option><option value="2" <?php if ($store_branch == 2) echo "selected=\"selected\"";?>>Yessamin</option><option value="3" <?php if ($store_branch == 3) echo "selected=\"selected\"";?> >Sta. Maria</option>
 </select></td>
 <td>Cutoff:</td><td><select id="fcutoff" style="width: 110px;"><option value="0">All</option><option value="1">First Cutoff</option><option value="2">End of the day</option></select></td>
-<td>Date:</td><td><input type="text" id="fdate" size="10" class="highlight" value="<?php echo date("m/d/Y",strtotime("+8 Hours")); ?>" /></td>
+<td>Date:</td><td><input type="text" id="fdate" size="10" class="highlight" value="<?php echo date("m/d/Y"); ?>" /></td>
 <td><input type="button" id="search" value="SEARCH"/></td>
 </tr>
 </table>
